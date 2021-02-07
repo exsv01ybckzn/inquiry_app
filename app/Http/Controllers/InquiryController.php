58082ebcd059ab5931f205a1ae9ea093f0f0fea3
Yuuki <exsv01ybckzn@gmail.com>
2021-02-07 20:14:56 +0900

    index 変更

---
+++ app/Http/Controllers/InquiryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InquiryController extends Controller
+{
+	//
+	public function entry(Request $request)
+	{
+		try
+		{
+			$inquiry = new Inquiry;
+
+			$inquiry->name = $request->input('name');
+
+			$inquiry->email = $request->input('email');
+
+			if(!empty($request->input('company')))
+			{
+				$inquiry->company = $request->input('company');
+			}
+
+			$inquiry->staffs = $request->input('staffs');
+
+			$inquiry->message = $request->input('message');
+
+		}
+		catch(\IlluminateDatabaseQueryException $e)
+		{
+			$errorcode = $e->errorInfo[1];
+
+			//重複エラー処理
+			if($errorcode == 1062)
+			{
+				return back()->withInput()->withErrors(['error_message'=>"既に該当するデータが保存されております。\n"]);
+			}
+		}
+		catch(\Exception $er)
+		{
+			return back()->withInput()->withErrors(['error_message'=>"入力データに誤りがある可能性があります。\n入力データの確認をお願い致します。"]);
+		}
+	}
+}
