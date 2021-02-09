6cc3ca4db23b4357a8072b55edb1ef6aedc7d63f
Yuuki <exsv01ybckzn@gmail.com>
2021-02-10 08:28:08 +0900

    デザイン修正　確認ページ

--- resources/views/content/confirm.blade.php
+++ resources/views/content/confirm.blade.php
@@ -15,35 +15,35 @@
 						@csrf
 						
 						<div class="form-group">
-							<label for="name">[氏名]</label><br>
+							<label for="name">氏名</label><br>
 							{{ $inputs['name'] }}
 							<input type="hidden" id="name" name="name" value="{{ $inputs['name'] }}">
 						</div>	
 						<div class="form-group">
-							<label for="email">[メールアドレス]</label><br>
+							<label for="email">メールアドレス</label><br>
 							{{ $inputs['email'] }}
 							<input type="hidden" id="email" name="email" value="{{ $inputs['email'] }}">
 						</div>
 						<div class="form-group">
-							<label for=="company">[会社名]</label><br>
+							<label for=="company">会社名</label><br>
 							{{ $inputs['company'] }}
 							<input type="hidden" id="company" name="company" value="{{ $inputs['company'] }}">
 
 						</div>
 						<div class="form-group">
-							<label for=="staffs">[担当者]</label><br>
+							<label for=="staffs">担当者</label><br>
 							{{ $inputs['staffs'] }}
 							<input type="hidden" id="staffs" name="staffs" value="{{ $inputs['staffs'] }}">
 
 						</div>
 						<div class="form-group">
-							<label for=="title">[案件・タイトル]</label><br>
+							<label for=="title">案件・タイトル</label><br>
 							{{ $inputs['title'] }}
 							<input type="hidden" id="title" name="title" value="{{ $inputs['title'] }}">
 
 						</div>
 						<div class="form-group">
-							<label for="body">[提案・要望・質問]</label><br>
+							<label for="body">提案・提案・要望・質問</label><br>
 							{!! nl2br(e($inputs['body'])) !!}
 							<input id="body" name="body" value="{{ $inputs['body'] }}" type="hidden">
 						</div>
