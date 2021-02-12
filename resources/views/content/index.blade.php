6b1f3dc0d5a3f7117692aae94c158758dd102502
Yuuki <exsv01ybckzn@gmail.com>
2021-02-12 11:25:07 +0900

    項目　修正

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -8,39 +8,15 @@
 			<div class="card">
 				<!-- 問い合わせ　内容 -->
 				<div class="card-header">
-					問い合わせフォーム
+					お客様要望内容　入力
 				</div>
 				<div class="card-body table-responsivei collapse show" id="inquiry_save">
 					<form action="{{ route('content.confirm') }}" method="POST" id="save">
 						@csrf
 
 						<div class="form-group">
-							<label for="name">氏名</label>
-							<input type="text" id="name" name="name" class="form-control" value="{{ old('name')}}">
-						
-							@if ($errors->has('name'))
-								<p class="error-message">{{ $errors->first('name') }}</p>
-							@endif
-						</div>	
-						<div class="form-group">
-							<label for="email">メールアドレス</label>
-							<input type="text" id="email" name="email" class="form-control" value="{{ old('email') }}">
-							@if ($errors->has('email'))
-								<p class="error-message">{{ $errors->first('email') }}</p>
-							@endif
-						</div>
-						<div class="form-group">
-							<label for=="company">会社名</label>
-							<input type="text" id="company" name="company" class="form-control" value="{{ old('company')}}">
-
-							@if ($errors->has('company'))
-								<p class="error-message">{{ $errors->first('company') }}</p>
-							@endif
-
-						</div>
-						<div class="form-group">
-							<label for=="staffs">担当者</label>
-							<input type="text" id="staffs" name="staffs" class="form-control" value="{{ old('staffs') }}" list="list">
+							<label for="staffs">営業担当者</label>
+							<input type="text" id="staffs" name="staffs" class="form-control" value="{{ old('staffs') }}">
 							@if (!empty($members))
 								<datalist id="list">
 									@foreach($members as $member)
@@ -55,7 +31,23 @@
 
 						</div>
 						<div class="form-group">
-							<label for=="title">案件・タイトル</label>
+							<label for="company">お客様名・施設名</label>
+							<input type="text" id="company" name="company" class="form-control" value="{{ old('company')}}">
+
+							@if ($errors->has('company'))
+								<p class="error-message">{{ $errors->first('company') }}</p>
+							@endif
+						</div>
+						<div class="form-group">
+							<label for="name">お客様　担当者</label>
+							<input type="text" id="name" name="name" class="form-control" value="{{ old('name')}}">
+						
+							@if ($errors->has('name'))
+								<p class="error-message">{{ $errors->first('name') }}</p>
+							@endif
+						</div>	
+						<div class="form-group">
+							<label for="title">案件・タイトル</label>
 							<input type="text" id="title" name="title" class="form-control" value="{{ old('title') }}">
 
 							@if ($errors->has('title'))
@@ -72,38 +64,10 @@
 							@endif
 						</div>
 
-						<button type="submit" class="btn btn-primary form-control">送信</button>
+						<button type="submit" class="btn btn-primary form-control">登録</button>
 
 					</form>
 				</div>
-
-				<br>
-
-				@if(!empty($histories))
-				<!-- 問い合わせ　履歴 -->
-				<div class="card-header">
-					問い合わせ　履歴
-				</div>
-				<div class="card-body table-responsivei collapse show" id="inquiry_save">
-					<table class="table table-borderless">
-						<thead>
-						</thead>
-						@foreach($histories as $history)
-						<body>
-							<tr class="table">
-								<td class="table-text">
-									<div>	
-										<p>日付:{{ date('Y-m-d', strtotime($history->created_at)) }}</p>
-										<p>案件:{{ $history->title }}</p>
-										<p>内容:{{ $history->body }}</p><hr>
-									</div>
-								</td>
-							</tr>
-						</body>
-						@endforeach
-					</table>
-				</div>
-				@endif
 			</div>
 		</div>
 	</div>
