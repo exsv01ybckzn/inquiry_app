c1739b0f9e056859e7be4a54a0cb9a2d3999a8f2
Yuuki <exsv01ybckzn@gmail.com>
2021-02-15 10:53:07 +0900

    ボタン追加

--- resources/views/content/thanks.blade.php
+++ resources/views/content/thanks.blade.php
@@ -3,5 +3,10 @@
 @section('content')
 	<div class="col-md-8">
 		<h5>送信が完了しました。</h5>
+		<br><br><br>
+		<form action="{{ route('content.entry') }}" method="POST">
+			<button type="submit" class="btn btn-primary" name="btn" value="back">ホーム</button>
+			<button type="submit" class="btn btn-primary" name="btn" value="next">履歴</button>
+		</form>
 	</div>
 @endsection
