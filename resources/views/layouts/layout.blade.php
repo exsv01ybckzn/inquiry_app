54a6bc46a37e5dcbba514b8d00f3a9c1f73e20e3
Yuuki <exsv01ybckzn@gmail.com>
2021-02-16 02:01:33 +0900

    ページレイアウト変更

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -32,8 +32,45 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-		<!-- content -->
-		@yield('content')
+		<!--  -->
+		<div id="app">
+			<nav class="navbar navbar-light bg-blue shadow-sm">
+				<div class="container">
+
+					<!-- Right Side Of Navbar -->
+					<ul class="navbar-nav ml-auto">
+						<!-- Authentication Links -->
+						<li class="nav-item dropdown">
+							<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+								{{ Auth::user()->name }} <span class="caret"></span>
+							</a>
+
+							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+								<a class="dropdown-item" href="{{ route('content.index') }}" type="submit">登録</a>
+								<a class="dropdown-item" href="{{ route('content.history') }}" type="submit">履歴</a>
+
+								<a class="dropdown-item" href="{{ route('logout') }}"
+									onclick="event.preventDefault();
+									document.getElementById('logout-form').submit();">
+									{{ __('Logout') }}
+								</a>
+
+								<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+								@csrf
+								</form>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</nav>
+
+			<main class="py-4">
+				<!-- content -->
+				@yield('content')
+			</main>
+		</div>
+
 		@yield('sccript')
 		@yield('style')
 
